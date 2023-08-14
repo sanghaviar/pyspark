5fd426d5ef7c0be3b97adac7b99dbe5b36a1aa26
sanghavi <sanghavi.a.r@diggibyte.com>
2023-08-14 13:28:39 +0530

    pyspark assignment1

--- Assignments/src/Assignment1/driver_assignment1.py
+++ Assignments/src/Assignment1/driver_assignment1.py
@@ -9,9 +9,11 @@ data = [('Washing Machine', "1648770933000", 20000, 'samsung', 'India', '0001'),
 schema = ['Product Name', 'Issue_Date', 'Price', 'Brand', 'Country', 'product_number']
 df = spark.createDataFrame(data, schema)
 time_stamp(df)
-# data1 = [(150711, 123456, 'EN', 456789, '2021-12-27T08:20:29.842+0000', '0001'),
-#                  (150439, 234567, 'UK', 345678, '2021-12-27T08:21:14.645+0000', '0002'),
-#                  (150647, 345678, 'ES', 234567, '2021-12-27T08:22:42.445+0000', '0003')]
-# schema1 = ['SourceId', 'TransactionNumber', 'Language', 'ModelNumber', 'StartTime', 'Product Number']
-# df1 = spark.createDataFrame(data1, schema1)
-# time_stamp(df1)
+output1 = time_stamp(df)
+
+data1 = [(150711, 123456, 'EN', 456789, '2021-12-27T08:20:29.842+0000', '0001'),
+                 (150439, 234567, 'UK', 345678, '2021-12-27T08:21:14.645+0000', '0002'),
+                 (150647, 345678, 'ES', 234567, '2021-12-27T08:22:42.445+0000', '0003')]
+schema1 = ['SourceId', 'TransactionNumber', 'Language', 'ModelNumber', 'StartTime', 'Product_Number']
+df1 = spark.createDataFrame(data1, schema1)
+transform(df1,output1)
--- Assignments/src/Assignment1/util_assignment1.py
+++ Assignments/src/Assignment1/util_assignment1.py
@@ -1,5 +1,6 @@
 from pyspark.sql.functions import *
 
+
 def time_stamp(df):
     """
     Convert the Issue Date with the timestamp format
@@ -23,37 +24,27 @@ def time_stamp(df):
     return tr
 
 
-# def transform(df1):
-#     rename = df1.withColumnRenamed('Product Name', 'product name') \
-#         .withColumnRenamed('Brand', 'brand') \
-#         .withColumnRenamed('Language', 'language') \
-#         .withColumnRenamed('ModelNumber', 'model_number') \
-#         .withColumnRenamed('StartTime', 'start_time') \
-#         .withColumnRenamed('Product Number', 'product number')
-#     # rename.show(truncate=False)
-#     df4 = rename.withColumn("start_time_ms", unix_timestamp(col("start_time"), "yyyy-MM-dd'T'HH:mm:ss.SSSZ") * 1000)
-#     df4.show(truncate=False)
+def transform(df1,output1):
+    rename = df1.withColumnRenamed('SourceId', 'source_id') \
+        .withColumnRenamed('TransactionNumber', 'transaction_number') \
+        .withColumnRenamed('Language', 'language') \
+        .withColumnRenamed('StartTime', 'start_time') \
+        .withColumnRenamed('Product_Number', 'product_number')
+    # rename.show(truncate=False)
+    df4 = rename.withColumn("start_time_ms", unix_timestamp(col("start_time"), "yyyy-MM-dd'T'HH:mm:ss.SSSZ") * 1000)
+    # df4.show(truncate=False)
+
+    #Joining two dataframes
+    df5 = df4.join(output1, df4.product_number == output1.product_number,'inner')
+
+    #filtering dataFrame
+    df6 = df5.filter(df5.language == 'EN')
+    return df6
 
 
 
 
 
 
-
-
-
-# spark = SparkSession.builder.getOrCreate()
-# data = [('Washing Machine', "1648770933000", 20000, 'samsung', 'India', '0001'),
-#                 ('Refrigerator', "1648770999000", 35000, 'LG', None, '0002'),
-#                 ('Air Cooler', "1648770948000", 45000, 'Voltas', None, '0003')]
-# schema = ['Product Name', 'Issue_Date', 'Price', 'Brand', 'Country', 'Product_Number']
-# df = spark.createDataFrame(data, schema)
-# data1 = [(150711, 123456, 'EN', 456789, '2021-12-27T08:20:29.842+0000', '0001'),
-#                  (150439, 234567, 'UK', 345678, '2021-12-27T08:21:14.645+0000', '0002'),
-#                  (150647, 345678, 'ES', 234567, '2021-12-27T08:22:42.445+0000', '0003')]
-# schema1 = ['SourceId', 'TransactionNumber', 'Language', 'ModelNumber', 'StartTime', 'Product_Number']
-# df1 = spark.createDataFrame(data1,schema1)
-# df1.join(df,df1.Product_Number == df.Product_Number,'inner').show()
-# # empDf.join(depDf, empDf.dep == depDf.id,'full').show()
 
 
